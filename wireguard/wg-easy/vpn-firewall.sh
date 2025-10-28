f3149955f36d40b0e8858e4d181a8e615b02b230
Topech <benlbttm0@gmail.com>
2025-10-28 14:29:28 +0000

    add jellyfin to vpn and improve it

--- wireguard/wg-easy/vpn-firewall.sh
+++ wireguard/wg-easy/vpn-firewall.sh
@@ -90,17 +90,32 @@ HOST_IP=$(getent hosts host.docker.internal | awk '{ print $1 }')
 
 # ----
 
-iptables -t nat ${ACTION_FLAG} PREROUTING -i wg0 -d 10.8.0.1 -p tcp --dport 22 \
-    -j DNAT --to-destination "${HOST_IP}"
+# ### Send traffic to the docker service's host machine
+# (prerouting so its differentiated from normal VPN traffic)
+iptables -t nat ${ACTION_FLAG} PREROUTING -i wg0 -d 10.8.0.1 -p tcp \
+	-m multiport --dports 22,5985,8096 \
+	-j DNAT --to-destination "${HOST_IP}"
+# iptables -t nat ${ACTION_FLAG} PREROUTING -i wg0 -d 10.8.0.1 -p tcp --dport 5985 \
+	# -j DNAT --to-destination "${HOST_IP}"
+
+
+# Accept VPN protocol
 iptables ${ACTION_FLAG} INPUT -p udp -m udp --dport 51820 -j ACCEPT
 
+# Allow VPN traffic to bridge from VPN network to LAN network
+iptables ${ACTION_FLAG} FORWARD -i wg0 -o eth0 -s 10.8.0.0/24 \
+	-m multiport --dports 22,5985,8096 \
+	-j ACCEPT
 
-# iptables ${ACTION_FLAG} FORWARD -i wg0 -j ACCEPT
-# iptables ${ACTION_FLAG} FORWARD -o wg0 -j ACCEPT
-iptables ${ACTION_FLAG} FORWARD -i wg0 -o eth0 -j ACCEPT
-iptables ${ACTION_FLAG} FORWARD -i eth0 -o wg0 -j ACCEPT
+# Only allow LAN network into VPN if its related to VPN traffic
+iptables ${ACTION_FLAG} FORWARD -i eth0 -o wg \
+	-m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables ${ACTION_FLAG} FORWARD -i eth0 -o wg \
+	-m state --state INVALID -j DROP
 
+# Any VPN traffic out looks like it comes from this docker service
 iptables -t nat ${ACTION_FLAG} POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+
 
 # drop any unexpected traffic on VPN network
 iptables ${ACTION_FLAG} INPUT -i wg0 -j DROP
