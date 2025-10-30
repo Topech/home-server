7e64cc72ac5add196198461abeb3f2668020371f
Topech <benlbttm0@gmail.com>
2025-10-30 01:54:28 +0000

    chore: cleanup old vpn firewall comments, add dns port

--- wireguard/wg-easy/vpn-firewall.sh
+++ wireguard/wg-easy/vpn-firewall.sh
@@ -14,89 +14,14 @@ fi
 # Resolve host.docker.internal to an IP
 HOST_IP=$(getent hosts host.docker.internal | awk '{ print $1 }')
 
-# Allow only VPN clients (wg0) to access host 5985 and 22
-# iptables ${ACTION_FLAG} INPUT -i wg0 -s 10.8.0.0/24 -d "${HOST_IP}" -p tcp --dport 5985 -j ACCEPT
-# iptables ${ACTION_FLAG} INPUT -i wg0 -s 10.8.0.0/24 -d "${HOST_IP}" -p tcp --dport 22 -j ACCEPT
-# 
-# # DNAT rules (VPN clients can reach host via 10.8.0.1)
-# iptables -t nat ${ACTION_FLAG} PREROUTING -i wg0 -d 10.8.0.1 -p tcp --dport 5985 \
-#     -j DNAT --to-destination "${HOST_IP}"
-# iptables -t nat ${ACTION_FLAG} PREROUTING -i wg0 -d 10.8.0.1 -p tcp --dport 22 \
-#     -j DNAT --to-destination "${HOST_IP}"
-# 
-# # Masquerade and allow VPN
-# iptables -t nat ${ACTION_FLAG} POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
-# ip6tables -t nat ${ACTION_FLAG} POSTROUTING -s fdcc:ad94:bacf:61a4::cafe:0/112 -o eth0 -j MASQUERADE
-# 
-# # Allow VPN packets (WireGuard)
-# iptables ${ACTION_FLAG} INPUT -p udp --dport 51820 -j ACCEPT
-# ip6tables ${ACTION_FLAG} INPUT -p udp --dport 51820 -j ACCEPT
-# 
-# # Drop any other forwarding from wg0 to the host
-# iptables ${ACTION_FLAG} FORWARD -i wg0 -j DROP
-
-
-# # Forward SSH (22) and CouchDB (5985) from VPN clients to the host
-# iptables -t nat ${ACTION_FLAG} PREROUTING -i wg0 -p tcp --dport 22   -j DNAT --to-destination ${HOST_IP}:22
-# iptables -t nat ${ACTION_FLAG} PREROUTING -i wg0 -p tcp --dport 5985 -j DNAT --to-destination ${HOST_IP}:5985
-# 
-# iptables ${ACTION_FLAG} FORWARD -i wg0 -o eth0 -p tcp -d ${HOST_IP} --dport 22   -j ACCEPT
-# iptables ${ACTION_FLAG} FORWARD -i wg0 -o eth0 -p tcp -d ${HOST_IP} --dport 5985 -j ACCEPT
-# 
-# # Masquerade replies so host sends responses back correctly
-# # iptables -t nat ${ACTION_FLAG} POSTROUTING -s 10.8.0.0/24 -d ${HOST_IP} -j MASQUERADE
-# iptables -t nat ${ACTION_FLAG} POSTROUTING -i wg0 -o eth0 -s 10.8.0.0/24 -d ${HOST_IP} -p tcp --dport 22 -j MASQUERADE
-# iptables -t nat ${ACTION_FLAG} POSTROUTING -i wg0 -o eth0 -s 10.8.0.0/24 -d ${HOST_IP} -p tcp --dport 5985 -j MASQUERADE
-# 
-# # Allow any incoming traffic that originated from VPN
-# iptables ${ACTION_FLAG} FORWARD -i eth0 -o wg \
-# 	-p tcp -d ${HOST_IP} --dport 22 \
-# 	-m state --state RELATED,ESTABLISHED -j ACCEPT
-# iptables ${ACTION_FLAG} FORWARD -i eth0 -o wg \
-# 	-p tcp -d ${HOST_IP} --dport 5985 \
-# 	-m state --state RELATED,ESTABLISHED -j ACCEPT
-# 
-# # drop any packets that dont match our rules
-# iptables -A FORWARD -m state --state INVALID -j DROP
-
-
-
 # ----
-  
-# allow input in
-# iptables ${ACTION_FLAG} INPUT -p udp --dport 51820 -j ACCEPT
-
-# iptables ${ACTION_FLAG} FORWARD -i wg0 -o eth0 -p -s 10.8.0.0/24 tcp -j ACCEPT
-# iptables ${ACTION_FLAG} FORWARD -i eth0 -o wg0 -p tcp -j ACCEPT
-
-# Any network traffic out looks as if its from this device
-# iptables -t nat ${ACTION_FLAG} POSTROUTING -o eth0 -s 10.8.0.0/24 -j MASQUERADE
-# iptables -t nat ${ACTION_FLAG} POSTROUTING -i wg0 -o eth0 -s 10.8.0.0/24 -d ${HOST_IP} -j MASQUERADE
-
-# # Allow any incoming traffic that originated from VPN
-# # (relies on strict VPN forwarding out regulation)
-# iptables ${ACTION_FLAG} FORWARD -i eth0 -o wg \
-# 	-m state --state RELATED,ESTABLISHED -j ACCEPT
-
-# iptables -A FORWARD -m state --state INVALID -j DROP
-
-# iptables ${ACTION_FLAG} FORWARD -i eth0 -o wg \
-# 	-p tcp -d ${HOST_IP} --dport 22 \
-# 	-m state --state RELATED,ESTABLISHED -j ACCEPT
-# iptables ${ACTION_FLAG} FORWARD -i eth0 -o wg \
-# 	-p tcp -d ${HOST_IP} --dport 5985 \
-# 	-m state --state RELATED,ESTABLISHED -j ACCEPT
-
-
-# ----
+# IPTABLE rules
 
 # ### Send traffic to the docker service's host machine
 # (prerouting so its differentiated from normal VPN traffic)
 iptables -t nat ${ACTION_FLAG} PREROUTING -i wg0 -d 10.8.0.1 -p tcp \
-	-m multiport --dports 22,5985,8096 \
+	-m multiport --dports 22,5985,8096,53 \
 	-j DNAT --to-destination "${HOST_IP}"
-# iptables -t nat ${ACTION_FLAG} PREROUTING -i wg0 -d 10.8.0.1 -p tcp --dport 5985 \
-	# -j DNAT --to-destination "${HOST_IP}"
 
 
 # Accept VPN protocol
@@ -104,7 +29,7 @@ iptables ${ACTION_FLAG} INPUT -p udp -m udp --dport 51820 -j ACCEPT
 
 # Allow VPN traffic to bridge from VPN network to LAN network
 iptables ${ACTION_FLAG} FORWARD -i wg0 -o eth0 -s 10.8.0.0/24 \
-	-m multiport --dports 22,5985,8096 \
+	-m multiport --dports 22,5985,8096,53 \
 	-j ACCEPT
 
 # Only allow LAN network into VPN if its related to VPN traffic
