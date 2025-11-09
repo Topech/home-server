458fc33b11ed12f19d6792cec86cb4182becc162
Topech <benlbttm0@gmail.com>
2025-11-09 14:00:23 +0000

    add vpn access to jellyfin

--- wireguard/wg-easy/vpn-firewall.sh
+++ wireguard/wg-easy/vpn-firewall.sh
@@ -20,7 +20,7 @@ HOST_IP=$(getent hosts host.docker.internal | awk '{ print $1 }')
 # ### Send traffic to the docker service's host machine
 # (prerouting so its differentiated from normal VPN traffic)
 iptables -t nat ${ACTION_FLAG} PREROUTING -i wg0 -d 10.8.0.1 -p tcp \
-	-m multiport --dports 22,5985 \
+	-m multiport --dports 22,5985,7074 \
 	-j DNAT --to-destination "${HOST_IP}"
 
 
@@ -29,7 +29,7 @@ iptables ${ACTION_FLAG} INPUT -p udp -m udp --dport 51820 -j ACCEPT
 
 # Allow VPN traffic to bridge from VPN network to LAN network
 iptables ${ACTION_FLAG} FORWARD -i wg0 -o eth0 -s 10.8.0.0/24 \
-	-m multiport --dports 22,5985 \
+	-m multiport --dports 22,5985,7074 \
 	-j ACCEPT
 
 # Only allow LAN network into VPN if its related to VPN traffic
